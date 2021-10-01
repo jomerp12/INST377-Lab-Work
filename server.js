db40b4e6a310b24e69877aafeb056552b7f1f872
jomerp12 <jpaulino@terpmail.umd.edu>
2021-10-01 09:12:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,9 @@ import connectReload from 'connect-livereload';
 import dotenv from 'dotenv';
 import path from 'path';
 
-<<
-<< << < HEAD
-import db from './server/database/initializeDB.js'; ===
-=== =
 dotenv.config();
 const __dirname = path.resolve();
-// import db from './server/database/initializeDB.js';
->>>
->>> > 17514e1 bb9519f84c7a2fb0ab664c5b893830434
+import db from './server/database/initializeDB.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,14 +17,9 @@ const staticFolder = 'client';
 let liveReloadServer;
 
 // Add some auto-reloading to our server
-if (process.env.CONTEXT === 'development') { <<
-    << << < HEAD
-    const liveReloadServer = reload.createServer();
-    liveReloadServer.watch(path.join(__dirname, staticFolder)); ===
-    === =
+if (process.env.CONTEXT === 'development') {
     liveReloadServer = reload.createServer();
-    liveReloadServer.watch(path.join(__dirname, staticFolder)); >>>
-    >>> > 17514e1 bb9519f84c7a2fb0ab664c5b893830434
+    liveReloadServer.watch(path.join(__dirname, staticFolder));
 }
 
 app.use((req, res, next) => {
@@ -42,45 +31,29 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(staticFolder)); <<
-<< << < HEAD
+app.use(express.static(staticFolder));
+// app.use('/template', labRoutes);
 app.use('/api', apiRoutes);
 
 async function bootServer() {
     try {
-        // Turn these back on in later labs
-        const mysql = await db.sequelizeDB;
-        await mysql.sync();
         app.listen(PORT, () => {
+            // Turn these back on in later labs
+            // const mysql = await db.sequelizeDB;
+            // await mysql.sync();
             console.log(`Listening on: http//localhost:${PORT}`);
+            console.log(`environment:`, process.env.CONTEXT);
         });
     } catch (err) {
         console.error(err);
-    } ===
-    === =
-    // app.use('/template', labRoutes);
-    app.use('/api', apiRoutes);
-
-    async function bootServer() {
-        try {
-            app.listen(PORT, () => {
-                // Turn these back on in later labs
-                // const mysql = await db.sequelizeDB;
-                // await mysql.sync();
-                console.log(`Listening on: http//localhost:${PORT}`);
-                console.log(`environment:`, process.env.CONTEXT);
-            });
-        } catch (err) {
-            console.error(err);
-        } >>>
-        >>> > 17514e1 bb9519f84c7a2fb0ab664c5b893830434
     }
+}
 
-    bootServer();
-    if (process.env.CONTEXT === 'development') {
-        liveReloadServer.server.once('connection', () => {
-            setTimeout(() => {
-                liveReloadServer.refresh('/');
-            }, 100);
-        });
-    }
+bootServer();
+if (process.env.CONTEXT === 'development') {
+    liveReloadServer.server.once('connection', () => {
+        setTimeout(() => {
+            liveReloadServer.refresh('/');
+        }, 100);
+    });
+}
