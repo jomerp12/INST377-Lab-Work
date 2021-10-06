4dee4eb690179bf6b9edb76675ba43a564cf6c84
jomerp12 <jpaulino@terpmail.umd.edu>
2021-10-06 15:20:15 -0400

    Lab 6 complete

---
+++ client/lab_6/script.js
@@ -0,0 +1,51 @@
+async function windowActions() {
+    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+
+    const request = await fetch(endpoint)
+    const cities = await request.json()
+
+    function findMatches(wordToMatch, cities) {
+        return cities.filter(place => {
+            // here we need to figure out if the city or state matches what was searched
+            const regex = new RegExp(wordToMatch, 'gi');
+            return place.name.match(regex) || place.category.match(regex)
+        });
+    }
+
+
+    function displayMatches() {
+        const matchArray = findMatches(event.target.value, cities);
+        const html = matchArray.map(place => {
+            const regex = new RegExp(event.target.value, 'gi');
+            const restaurantName = place.name.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            const addressLine = place.address_line_1.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            const cityName = place.city.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            const stateName = place.state.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            const zipCode = place.zip.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            return `
+        <li>
+        <span class="name">${restaurantName}</span>
+        <br>
+        <span class="name">${addressLine}</span>
+        <br>
+        <span class="name">${cityName}, ${stateName}</span>
+        <br>
+        <span class="name">${zipCode}</span>
+        <br>
+        </li>
+        `
+        }).join('');
+        suggestions.innerHTML = html;
+    }
+
+    const searchInput = document.querySelector('.search');
+    const suggestions = document.querySelector('.suggestions');
+
+    searchInput.addEventListener('change', displayMatches);
+    searchInput.addEventListener('keyup', (evt) => {
+        displayMatches(evt)
+    });
+
+}
+window.onload = windowActions;
