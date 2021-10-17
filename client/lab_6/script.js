8679ce99d8bb28d2c55f50cd6d3ec0bb2fe46cec
jomerp12 <jpaulino@terpmail.umd.edu>
2021-10-17 13:19:11 -0400

    fixed lab 5 and lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -5,26 +5,25 @@ async function windowActions() {
     const request = await fetch(endpoint)
     const cities = await request.json()
 
-    async function dataHandlerfunction() {
+    function findMatches(wordToMatch, cities) {
+        return cities.filter(place => {
+            // here we need to figure out if the city or state matches what was searched
+            const regex = new RegExp(wordToMatch, 'gi');
+            return place.name.match(regex) || place.category.match(regex)
+        });
+    }
 
-        function findMatches(wordToMatch, cities) {
-            return cities.filter(place => {
-                // here we need to figure out if the city or state matches what was searched
-                const regex = new RegExp(wordToMatch, 'gi');
-                return place.name.match(regex) || place.category.match(regex)
-            });
-        }
 
-        function displayMatches() {
-            const matchArray = findMatches(event.target.value, cities);
-            const html = matchArray.map(place => {
-                const regex = new RegExp(event.target.value, 'gi');
-                const restaurantName = place.name.replace(regex, `<span class="hl">${event.target.value}</span>`);
-                const addressLine = place.address_line_1.replace(regex, `<span class="hl">${event.target.value}</span>`);
-                const cityName = place.city.replace(regex, `<span class="hl">${event.target.value}</span>`);
-                const stateName = place.state.replace(regex, `<span class="hl">${event.target.value}</span>`);
-                const zipCode = place.zip.replace(regex, `<span class="hl">${event.target.value}</span>`);
-                return `
+    function displayMatches() {
+        const matchArray = findMatches(event.target.value, cities);
+        const html = matchArray.map(place => {
+            const regex = new RegExp(event.target.value, 'gi');
+            const restaurantName = place.name.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            const addressLine = place.address_line_1.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            const cityName = place.city.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            const stateName = place.state.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            const zipCode = place.zip.replace(regex, `<span class="hl">${event.target.value}</span>`);
+            return `
         <li>
         <span class="name">${restaurantName}</span>
         <br>
@@ -36,17 +35,17 @@ async function windowActions() {
         <br>
         </li>
         `
-            }).join('');
-            suggestions.innerHTML = html;
-        }
+        }).join('');
+        suggestions.innerHTML = html;
     }
-    const searchInput = document.querySelector('.search');
-    const suggestions = document.querySelector('.suggestions');
+}
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
-    searchInput.addEventListener('change', displayMatches);
-    searchInput.addEventListener('keyup', (evt) => {
-        displayMatches(evt)
-    });
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', (evt) => {
+    displayMatches(evt)
+});
 
 }
 window.onload = windowActions;
